70c46f6f9e532b6a57449cb81d62ecf6afb563c9
Nikita Kolosov <nikita-kolosov@yandex.ru>
2026-07-06 23:38:23 -0400

    feat: add Publications page

--- src/pages/Publications.tsx
+++ src/pages/Publications.tsx
@@ -1,1 +1,18 @@
-export default function Publications() { return null }
+import { publications } from '../data/publications'
+import { PublicationItem } from '../components/PublicationItem'
+import { Seo } from '../components/Seo'
+
+export default function Publications() {
+  const sorted = [...publications].sort((a, b) => b.year - a.year)
+  return (
+    <section>
+      <Seo title="Publications · Nikita Kolosov" />
+      <h1>Publications</h1>
+      <ul style={{ padding: 0, margin: 0 }}>
+        {sorted.map((pub) => (
+          <PublicationItem key={pub.title} pub={pub} />
+        ))}
+      </ul>
+    </section>
+  )
+}
